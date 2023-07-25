966f0a135c4de9b52bf8042541e3cbabf3699c20
ildadu <litvilyanot300@mail.ru>
2023-07-25 18:40:20 +0300

    services.php

--- services.php
+++ services.php
@@ -1,11 +1,11 @@
 <?php 
 require_once 'config/connect.php';
 // require_once 'index.php';
+$texts = mysqli_query($connect, "SELECT * FROM `texts`");
+$texts = mysqli_fetch_all($texts);
 ?>
 <pre>
     <?php
-    $texts = mysqli_query($connect, "SELECT * FROM `texts`");
-    $texts = mysqli_fetch_all($texts);
     print_r($texts);
     ?>
 </pre>
