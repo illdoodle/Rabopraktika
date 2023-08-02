7e194116be19b66fcf7a58f831b7b6f856c5f520
ildadu <litvilyanot300@mail.ru>
2023-08-02 19:23:40 +0300

    auth fix

--- auth/do_login.php
+++ auth/do_login.php
@@ -4,11 +4,11 @@ require_once '../config/connect.php';
 session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
-$_SESSION['username'] = $username;
 
 $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
 $stmt = mysqli_query($connect, $sql);
 if(mysqli_num_rows($stmt) > 0){
+    $_SESSION['username'] = $username;
     echo 'Успешная авторизация!', '<br>';
     ?>
     <a href="../index.php">На Заглавную</a>
