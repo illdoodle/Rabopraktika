3f1ba90f2ea985298ef298b115a934b6d6017dce
ildadu <litvilyanot300@mail.ru>
2023-08-04 18:49:17 +0300

    services fix

--- services.php
+++ services.php
@@ -1,12 +1,7 @@
 <?php 
 require_once 'config/connect.php';
-// $query = $connect->query("SELECT * FROM `texts`");
-// $texts = $query->fetchAll(PDO::FETCH_ASSOC);
-
 $texts = mysqli_query($connect, "SELECT * FROM `texts`");
 $texts = mysqli_fetch_all($texts);
-?>
-<?php
 function GetInnerByName($name, $texts){
     foreach($texts as $text){
         foreach($text as $textName){
